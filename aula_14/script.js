0d5bb98f7e0a2bb2c048328200ed5511b91a2e78
Thomas Lincoln <thmas5952@gmail.com>
2022-08-28 18:24:47 -0300

    aula 14

---
+++ aula_14/script.js
@@ -0,0 +1,44 @@
+// strings
+const texto = "oi cara, ta tudo bem com você?"
+
+console.log(texto.replace('a', 'b'));
+
+//strings são indexados
+// "texto"
+//  01234
+
+/*
+--- Funções ---
+
+indexOf("a")
+busca "a" desde o indice 0 até o ultimo, assim q achar ele retorna o primeiro.
+
+lastIndexOf("a")
+
+o mesmo só que o indexOf, mas ele parte do final
+
+em ambos é possível passar um valor de início
+
+("a", 3)
+
+
+.replace
+
+ele altera um texto por outro
+
+mas só funciona no primeiro encontrado
+
+
+.lenght 
+
+informa o tamanho da string
+
+.slice
+
+ele retorna um trecho do texto
+
+ex = (2, 6)
+
+números negativos indicam q ele vai partir do último valor
+
+*/
