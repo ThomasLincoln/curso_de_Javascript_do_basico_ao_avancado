13c24542a56b561ec1b92303ae1853a83a7107e5
Thomas Lincoln <thmas5952@gmail.com>
2022-08-28 15:13:41 -0300

    aula 10

---
+++ aula_10/index.js
@@ -0,0 +1,14 @@
+// Tipos de dados primitivos
+
+const nome =  'Luiz' //string
+const num01 = 10 // number
+const num02 = 10.3 // number
+let aluno; //undefined -> não aponta pra lugar da memória
+let nomeAluno = null; // nulo -> não aponta pra lugar da memória
+const boolean = true; //boolean
+const boolean_2 = false; //boolean
+
+
+// é possivel descobrir o tipo de uma var usando typeof
+
+console.log (typeof nome, nome)
