041084d9e470b6a5e34759062da50cad13fa406c
Thomas Lincoln <thmas5952@gmail.com>
2022-08-28 15:35:54 -0300

    aula 11

---
+++ aula_11/index.js
@@ -0,0 +1,22 @@
+/* 
+Operadores Aritméticos
++ -> adição e concatenação
+-, /, * -> operações
+% -> resto da divisão
+** -> potenciação
+*/
+
+const num1 = 2;
+const num2 = 10;
+
+console.log(num1 % num2)
+
+// para converter uma string de numero 
+
+const teste = Number('23')
+console.log(teste)
+
+let contador = 1; 
+console.log(contador)
+contador ++;
+console.log(contador)
