f15cc6aad3bf27b1b635a6d821326c0840cd5830
Thomas Lincoln <thmas5952@gmail.com>
2022-08-28 16:04:20 -0300

    Aula 12

---
+++ aula_12/js/index.js
@@ -0,0 +1,9 @@
+alert("oi cara, relaxa ai, vai praticando")
+
+const aux1 = prompt("Digite o primeiro número: ");
+const aux2 = prompt("Digite o segundo número: ");
+
+const numero1 = Number(aux1);
+const numero2 = Number(aux2);
+
+alert(`O resultado da sua operação é: ${numero1+numero2}`)
