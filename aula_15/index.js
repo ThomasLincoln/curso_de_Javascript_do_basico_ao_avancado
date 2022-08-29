ba254f0c18ce21f50236c04722dc1cf6c37b12a6
Thomas Lincoln <thmas5952@gmail.com>
2022-08-29 19:32:20 -0300

    aula 15

---
+++ aula_15/index.js
@@ -0,0 +1,13 @@
+let num1 = '0.7';
+let num2 = 0.1;
+
+console.log(num1 + num2)
+
+// console.log(num1.toString() + num2)
+// num1 = num1.toString();
+// console.log(num1.toString())
+// console.log(typeof num1);
+
+let num3 = 12.923211342
+// console.log(num3.toFixed(2))
+
