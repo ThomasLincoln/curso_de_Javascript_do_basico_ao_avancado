05aeaf7cb83b18aa0ab95b0e33571ac3fcbdbcf7
Thomas Lincoln <thmas5952@gmail.com>
2022-08-30 12:10:49 -0300

    aula 16

---
+++ aula_16/script.js
@@ -0,0 +1,15 @@
+// Math //
+
+//arredondar
+
+let num1 = 9.321738;
+// let num2 = Math.floor(num1) //arredonda pra baixo
+// let num2 = Math.ceil(num1) //arredonda pra cima
+let num2 = Math.round(num1) //arredonda pro mais próximo
+console.log(num2)
+
+// encontrar o maior
+// console.log(Math.max(1,2,3,19,92,19,-1)); //pega o maior
+// console.log(Math.min(1,2,3,19,92,19,-1)); //pegar o menor
+
+console.log(Math.random(Math.random() * (10 - 5)))
