4972f5754928078ee79c13f32a28383bc5ea3c46
Thomas Lincoln <thmas5952@gmail.com>
2022-08-30 12:57:47 -0300

    aula 17

---
+++ aula_17/script.js
@@ -0,0 +1,26 @@
+//arrays
+
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+console.log(alunos)
+
+
+
+
+// console.log(alunos.slice(0, -1))
+
+// delete alunos[1]
+// console.log(alunos[1])
+
+
+// const removido = alunos.shift() //remove do começo
+// const removido = alunos.pop() //tira o do último
+// console.log(removido)
+
+// alunos.unshift("Luiza") // adc no começo
+// alunos.unshift("Otávio")// adc no começo
+
+
+// alunos.push("Otávio") //adc no fim
+// alunos.push("Luana")  //adc no fim
