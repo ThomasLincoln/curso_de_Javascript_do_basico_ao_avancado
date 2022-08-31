d62b2e27426067aa0d4330abf85e4dac543e81db
Thomas Lincoln <thmas5952@gmail.com>
2022-08-31 12:10:30 -0300

    Aula 19

---
+++ aula_19/script.js
@@ -0,0 +1,37 @@
+// const pessoa1 = {
+//     nome: 'Luiz', 
+//     sobrenome: 'Miranda', 
+//     idade: 25, 
+
+//     fala(){
+//         console.log(`Olá eu sou ${this.nome}`)
+//     }
+// }
+
+// pessoa1.fala()
+
+//-----------------------------------------------------------
+//função para criar objetos
+
+// function criaPessoa(nome, sobrenome, idade){
+//     return {
+//         nome,sobrenome, idade
+//     };
+// }
+
+// const pessoa1 = criaPessoa("Thomas", "Lincoln", 19)
+// const pessoa2 = criaPessoa("Maria", "Lincoln", 149)
+// const pessoa3 = criaPessoa("João", "Lincoln", 129)
+// const pessoa4 = criaPessoa("Júnior", "Lincoln", 319)
+// const pessoa5 = criaPessoa("Carlos", "Lincoln", 139)
+// console.log(pessoa1.nome, pessoa2.nome, pessoa3.nome)
+
+//--------------------------------------------------------------
+
+// const pessoa1 = {
+//     nome: 'Thomas',
+//     sobrenome: 'Lincoln', 
+//     idade: 25
+// }; 
+
+// console.log(pessoa1.idade)
