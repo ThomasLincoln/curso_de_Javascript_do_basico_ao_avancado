2da2ffe6dc18791a4af1aa60160861514e5c2b0b
Thomas Lincoln <thmas5952@gmail.com>
2022-09-11 15:20:43 -0300

    aula 29 for in

---
+++ aula_29/index.js
@@ -0,0 +1,13 @@
+const frutas = ['Pera', 'Maçã', 'Uva'];
+
+const pessoa = {
+    nome: 'Thomas',
+    sobrenome: 'Lincoln',
+    idade: 19
+}
+
+//for in
+
+for(let chave in pessoa){
+    console.log(pessoa[chave])
+}
