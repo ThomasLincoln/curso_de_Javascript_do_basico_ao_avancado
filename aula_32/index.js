28b839cf20e3e6b60a24a2bf9a99e5d40443b880
Thomas Lincoln <66880989+ThomasLincoln@users.noreply.github.com>
2022-09-12 18:30:33 -0300

    aula 32

---
+++ aula_32/index.js
@@ -0,0 +1,21 @@
+// while e do while
+
+function random (min, max){
+    const r = Math.random() * (max -min) + min;
+    return parseInt(r);
+}
+
+let rand = random(1, 50);
+
+
+while (rand !== 10){
+    rand = random(1, 50);
+    console.log(rand);
+}
+
+console.log('-------');
+
+do{
+    rand = random(1,50);
+    console.log(rand)
+}while(rand !== 10)
