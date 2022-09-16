a93e9e37c320cab98962e1f198f77f9165d2e6fd
Thomas Lincoln <66880989+ThomasLincoln@users.noreply.github.com>
2022-09-16 17:31:20 -0300

    aula 37
    tratamento de erro

---
+++ aula_37/index.js
@@ -0,0 +1,8 @@
+// tratamento de erro
+try{
+    console.log(naoExisto)
+}catch(erro){
+    console.log(erro)
+}
+
+//basicamente se ocorrer um erro no bloco try ele retorna o catch, se não, ele só executa o try
