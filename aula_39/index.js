4b4eb7930ac99e44ba9524f11bc0808b053e36a3
Thomas Lincoln <66880989+ThomasLincoln@users.noreply.github.com>
2022-09-16 18:03:10 -0300

    setTimeout e setInterval

---
+++ aula_39/index.js
@@ -0,0 +1,14 @@
+function mostraHora(){
+    let data = new Date()
+    return data.toLocaleTimeString('pt-BR', {
+
+    })
+}
+
+const timer = setInterval(function(){
+    console.log(mostraHora())
+}, 1000)
+
+setTimeout(function(){
+    clearInterval(timer)
+}, 3000)
