ba0a8f2975c61d6aa1e09f65304e48084a4dde59
Thomas Lincoln <66880989+ThomasLincoln@users.noreply.github.com>
2022-09-17 18:54:19 -0300

    exercicio finalizado

--- exercicio_timer/assets/js/main.js
+++ exercicio_timer/assets/js/main.js
@@ -3,26 +3,27 @@ const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
-function numeroAEsquerda(num){
-    return num >= 10 ? num : `0${num}`
-}
 let seg = 0
 let min = 0
 let hr = 0
+let timer;
+let data = new Date()
 
-pausar.addEventListener('click', function(event){
-    relogio.style.color = 'red'
-    parar()
-});
+function numeroAEsquerda(num){
+    return num >= 10 ? num : `0${num}`
+}
 
 iniciar.addEventListener('click', function(event){
-        contar(seg,min, hr )
+    controladora('contar')
 });
 
+pausar.addEventListener('click', function(event){
+
+    controladora('parar')
+});
 
 zerar.addEventListener('click', function(event){
-    seg = 0;
-    min = 0;
+    controladora('zerar')
 });
 
 
@@ -32,28 +33,51 @@ function mostraHora(data){
     let minuto = numeroAEsquerda(data.getMinutes())
     let segundo = numeroAEsquerda(data.getSeconds())
     return `${hora}:${minuto}:${segundo}`
-    
 }
 
 
 
-function contar(seg, min, hr){
-    setInterval(function(){
-        if(hr == 23 && min == 59 && seg == 58){
-            parar()
-        }
-        if(seg<59){
-            seg++
-        }else if(min < 59){
-            min++
-            seg = 0
-        }else{
-            hr++
-            min = 0
-            seg = 0
-        }
-        let data = new Date(`2020-12-20 ${hr}:${min}:${seg}`)
-        console.log(mostraHora(data))
-        relogio.textContent = mostraHora(data)
+function controladora(acao){
+    if(acao == 'contar'){
+        clearInterval(timer)
+        contar()
+        relogio.style.color = 'white'
+    }else if(acao == 'zerar'){
+        clearInterval(timer)
+        hr = 0; seg = 0; min = 0;
+        relogio.textContent = '00:00:00'
+        relogio.style.color = 'white'
+    }else if(acao == 'parar'){
+        clearInterval(timer)
+        relogio.style.color = 'red'
+    }
+}
+
+function attHora(segundo){
+    if(hr == 23 && min == 59 && segundo == 58){
+        parar()
+    }
+    if(segundo<59){
+        segundo++
+    }else if(min < 59){
+        min++
+        segundo = 0
+    }else{
+        hr++
+        min = 0
+        segundo = 0
+    }
+    let data = new Date(`2020-12-20 ${hr}:${min}:${segundo}`)
+    return data
+
+}
+
+function contar(){
+    timer = setInterval(function(){
+        dataAgora = attHora(seg)
+        console.log(mostraHora(dataAgora))
+        relogio.textContent = mostraHora(dataAgora)
+        seg++
     }, 1000)
 }
+
