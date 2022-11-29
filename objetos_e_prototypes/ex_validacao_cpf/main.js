fc73e173756cea3165cd596d90872e98db4da16e
Thomas Lincoln <thmas5952@gmail.com>
2022-11-29 09:55:29 -0300

    ex_validacao_cpf_my_solution

--- objetos_e_prototypes/ex_validacao_cpf/main.js
+++ objetos_e_prototypes/ex_validacao_cpf/main.js
@@ -1,5 +1,6 @@
 // 70548445052
 function verifica (texto, N){
+    // if(sequencia(texto))
     let mult = N;
     let soma = 0;
     for(let i = 0; i < N-1; i++){
@@ -13,21 +14,49 @@ function verifica (texto, N){
     return aux;
 }
 
+function verifica_constante(numero){
 
-
-const cpf = 70548445052;
-const prova = cpf % 100;
-
-let texto = cpf.toString();
-let cpf_aux = cpf;
-cpf_aux -= cpf % 100;
-let soma = verifica(texto, 10) * 10;
-cpf_aux += soma;
-texto = cpf_aux.toString();
-soma += verifica(texto, 11);
-
-if(soma === prova){
-    console.log("cpf válido");
-}else{
-    console.log("cpf inválido");
+    for(let i = 1; i<10; i++){
+        // let mult = pow(10, 2);
+        let numerounidade = numero % mult;
+        console.log(`casa número ${i} tem valor: ${numerounidade}`);
+    }
 }
+
+
+function verificaCpf(cpf_input){
+    let cpf_texto = cpf_input.replace(/\D+/g, '');
+    cpf_numero = parseInt(cpf_texto);
+    // verifica_constante(cpf_numero);
+    // if(verifica_constante(cpf_numero)){
+    //     return console.log("números repetidos não são permitidos");
+    // }
+
+    const prova = cpf_numero % 100;
+    let cpf_aux = cpf_numero;
+    cpf_aux -= cpf_numero % 100;
+    
+    if(cpf > 99999999999){
+        return console.log('erro');
+    }
+
+    
+    let soma = verifica(cpf_texto, 10) * 10;
+    cpf_aux += soma;
+    soma += verifica(cpf_texto, 11);
+    
+    
+    if(soma === prova){
+        console.log("cpf válido");
+    }else{
+        console.log("cpf inválido");
+    }
+    
+}
+
+const cpf = '705.484.450-52';
+const num = 1110223;
+
+// console.log(num % 10)
+// verifica_constante(num);
+verificaCpf(cpf);
