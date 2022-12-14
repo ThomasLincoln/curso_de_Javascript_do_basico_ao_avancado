32570e4ed0bf4d28372efacc2050a421bdc9ecac
Thomas Lincoln <thmas5952@gmail.com>
2022-12-14 08:25:47 -0300

    exercicio valida_cpf

---
+++ classes_orientacao_a_objetos/valida_cpf/index.js
@@ -0,0 +1,58 @@
+
+
+class ValidaCpf{
+    constructor(cpfEnviado){
+        Object.defineProperty(this, 'CpfLimpo',{
+            get: function(){
+                return cpfEnviado.replace(/\D+/g, '');
+            }
+        });
+    }
+
+    valida() {
+        if(typeof this.CpfLimpo === 'undefined'){
+            return false;
+        }
+    
+        if(this.CpfLimpo.length !== 11){
+            return false;
+        }
+    
+        if(this.isSequencia()){
+            return false;
+        }
+    
+        const cpfParcial = this.CpfLimpo.slice(0, -2);
+        const digito1 = this.criaDigito(cpfParcial);
+        const digito2 = this.criaDigito(cpfParcial + digito1);
+        // console.log(digito1);
+        // console.log(digito2);
+        const novoCpf = cpfParcial + digito1 + digito2;
+        // console.log(novoCpf);
+        return novoCpf === this.CpfLimpo;
+    }
+
+    criaDigito(cpfParcial){
+        const cpfArray = Array.from(cpfParcial);
+        // console.log(cpfArray);
+        // let somador;
+        let regressivo = cpfArray.length + 1;
+        let total = cpfArray.reduce((ac, val) =>{
+            ac += (regressivo * Number(val));
+            regressivo--;
+            return ac;
+        },0)
+        const digito = 11 - (total % 11);
+        return digito > 9 ? 0 : digito;
+    }
+
+    isSequencia(){
+        const sequencia = this.CpfLimpo[0].repeat(this.CpfLimpo.length);
+        return (sequencia === this.cpfLimpo);
+    }
+}
+
+
+const cpf = new ValidaCpf('070.987.720-03');
+// console.log(cpf.CpfLimpo);
+console.log(cpf.valida());
