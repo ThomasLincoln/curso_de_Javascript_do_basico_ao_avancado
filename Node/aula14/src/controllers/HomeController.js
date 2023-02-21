dc5b1330c7303620d3bc54cbb43a88788f44e896
Thomas <thmas5952@gmail.com>
2023-02-21 12:03:02 -0300

    Middlewares e Mongodb

---
+++ Node/aula14/src/controllers/HomeController.js
@@ -0,0 +1,10 @@
+
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+    return;
+}
+
+exports.trataPost = (req,res)=>{
+    res.send(req.body);
+    return;
+}
---
+++ Node/aula14/src/models/HomeModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+    titulo: {type: String, required: true},
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
