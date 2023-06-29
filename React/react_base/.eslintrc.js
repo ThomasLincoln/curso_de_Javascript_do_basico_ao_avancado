45d0011233ecd36b20f8681bdac0f4cee08ec891
Thomas Lincoln <thmas5952@gmail.com>
2023-06-29 20:15:33 -0300

    create base react app

---
+++ React/react_base/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ React/react_base/src/App.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+export default App;
