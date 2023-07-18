cf39aa452c643329533cec66cb3d2c7b6b1c592d
ThomasLincoln <thmas5952@gmail.com>
2023-07-18 14:05:04 -0300

    Aula 210

--- React/react_base/src/App.js
+++ React/react_base/src/App.js
@@ -1,19 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+
+import History from './services/history';
 import GlobalStyle from './styles/GlobalStyle';
 import Header from './components/Header';
-
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={History}>
       <Header />
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </Router>
   );
 }
 
