aa5d9f5f2e06e946b19c967d7125914529617c0a
ThomasLincoln <thmas5952@gmail.com>
2023-07-19 14:31:32 -0300

    Aula 214

--- React/react_base/src/routes/index.js
+++ React/react_base/src/routes/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
-  toast.error('oie');
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
