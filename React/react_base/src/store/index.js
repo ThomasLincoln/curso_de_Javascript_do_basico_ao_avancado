76a179413f1ac6f07321e5518f429609287f4f5c
ThomasLincoln <thmas5952@gmail.com>
2023-08-08 13:56:03 -0300

    Aula 216

--- React/react_base/src/store/index.js
+++ React/react_base/src/store/index.js
@@ -1,26 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { legacy_createStore as createStore } from 'redux';
 
-const initialState = {
-  botaoClicado: false,
-};
+import rootReducer from './modules/rootReducer';
 
-// reducer que irá ouvir ações e criar novos estados
-// eslint-disable-next-line default-param-last
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'BOTAO_CLICADO': {
-      const newState = { ...state };
-      newState.botaoClicado = !newState.botaoClicado;
-      return newState;
-    }
-
-    default: {
-      return state;
-    }
-  }
-};
-
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 export default store;
